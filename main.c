95bfb10735275520fc2fd4b5618ec75f355f520e
Mohamed Lamine Hachim <hachimdiop6@gmail.com>
2026-08-16 01:41:21 +0200

    discovery of GLII function. Its quiet good but the program ends directly after runing so the exit will still be ms. So, further, I have to make the program run anytime up to abort.

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
-    
+#include <windows.h>
+#include <stdio.h>
+
+int main() {
+    LASTINPUTINFO lii;
+    lii.cbSize = sizeof(LASTINPUTINFO);
+
+    if (GetLastInputInfo(&lii)) {
+        DWORD dwGetTickCount = GetTickCount();
+        DWORD idleTime = dwGetTickCount - lii.dwTime;
+        printf("Inactivité : %lu ms\n", idleTime);
+    } else {
+        printf("Erreur lors de l'appel\n");
+    }
+    return 0;
+}
